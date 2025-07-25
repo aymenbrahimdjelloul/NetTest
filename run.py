6570dfe260ee3bac3b7de829e5e4da3e99014a34
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-07-25 14:39:08 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+"""
+
+@author : Aymen Brahim Djelloul
+@version : 0.1
+@date : 23.07.2025
+@license : MIT License
+
+
+"""
+
+# IMPORTS
+from nettest._interface import main
+
+if __name__ == "__main__":
+    main()
